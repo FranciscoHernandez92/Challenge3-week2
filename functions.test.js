22836bd6a533838076f367629ef9c9036edadd8c
fran hernandez <franciscohernandezlopez5@gmail.com>
2024-03-06 20:45:14 +0100

    Add method shift

--- functions.test.js
+++ functions.test.js
@@ -1,4 +1,4 @@
-import { methodLength, methodPop } from './functions.js';
+import { methodLength, methodPop, methodShift } from './functions.js';
 
 describe('methodLength', () => {
   test('should be 0 when argument is []', () => {
@@ -30,12 +30,34 @@ describe('methodPop', () => {
     //Assert
     expect(r).toBe(expected);
   });
+
   test('should be "hola" when argument is ["mundo", "hola"]', () => {
     //Arrange
     const x = ['mundo', 'hola'];
     const expected = 'hola';
     //Act
     const r = methodPop(x);
+    //Assert
+    expect(r).toBe(expected);
+  });
+});
+
+describe('methodShift', () => {
+  test('should be 1 when argument is [1, 2, 3, 4]', () => {
+    //Arrange
+    const x = [1, 2, 3, 4];
+    const expected = 1;
+    //Act
+    const r = methodShift(x);
+    //Assert
+    expect(r).toBe(expected);
+  });
+  test('should be "hola" when argument is ["hola", "mundo"]', () => {
+    //Arrange
+    const x = ['hola', 'mundo'];
+    const expected = 'hola';
+    //Act
+    const r = methodShift(x);
     //Assert
     expect(r).toBe(expected);
   });
